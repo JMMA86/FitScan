b10816c6b906d32e0d7849e8a274826d5752232a
OscarGomez-jpg <os.gomez.lozano@gmail.com>
2025-04-28 16:54:35 -0500

    fix: changed the retrofit base_url

--- app/src/main/java/icesi/edu/co/fitscan/features/auth/data/remote/RetrofitInstance.kt
+++ app/src/main/java/icesi/edu/co/fitscan/features/auth/data/remote/RetrofitInstance.kt
@@ -8,7 +8,7 @@ import java.util.concurrent.TimeUnit
 
 object RetrofitInstance {
     // Cambia esta URL por la de tu servidor (ip local o dominio)
-    private const val BASE_URL = "https://24de-186-144-44-127.ngrok-free.app"
+    private const val BASE_URL = "https://fitscan.onrender.com/"
 
     private val loggingInterceptor = HttpLoggingInterceptor().apply {
         level = HttpLoggingInterceptor.Level.BODY
