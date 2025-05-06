17510de90361b014f2a7c767a5bd59968f9d9df6
OscarGomez-jpg <os.gomez.lozano@gmail.com>
2025-05-06 10:33:39 -0500

    feat: Pushing dimensions object to standarize the measures

---
+++ app/src/main/java/icesi/edu/co/fitscan/ui/theme/Dimensions.kt
@@ -0,0 +1,20 @@
+package icesi.edu.co.fitscan.ui.theme
+
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+object Dimensions {
+    val SmallPadding = 8.dp
+    val MediumPadding = 16.dp
+    val LargePadding = 24.dp
+
+    val SmallCornerRadius = 4.dp
+    val MediumCornerRadius = 8.dp
+    val LargeCornerRadius = 16.dp
+
+    val SmallTextSize = 12.sp
+    val MediumTextSize = 16.sp
+    val LargeTextSize = 20.sp
+
+    val buttonBorderRadius = 8.dp
+}
