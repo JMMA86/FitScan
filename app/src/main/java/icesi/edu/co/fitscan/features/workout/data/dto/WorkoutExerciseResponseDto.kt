254de5e1a0ef7d833a1059506bd92635078ca59c
Danna-Lopez-M <1109115195@u.icesi.edu.co>
2025-06-12 14:19:33 -0500

    Merge

--- app/src/main/java/icesi/edu/co/fitscan/features/workout/data/dto/WorkoutExerciseResponseDto.kt
+++ app/src/main/java/icesi/edu/co/fitscan/features/workout/data/dto/WorkoutExerciseResponseDto.kt
@@ -1,5 +1,5 @@
 package icesi.edu.co.fitscan.features.workout.data.dto
 
-data class WorkoutExerciseListResponseDTO(
+data class WorkoutExerciseListResponseDto(
     val data: List<WorkoutExerciseDto>? = null
 )
