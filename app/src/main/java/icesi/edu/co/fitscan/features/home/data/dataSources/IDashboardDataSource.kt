3a0d2cf77d181a092830a1e98b86d7088ceee273
OscarGomez-jpg <os.gomez.lozano@gmail.com>
2025-06-12 00:40:23 -0500

    fix: dashboard list didn't mapped workout properly

--- app/src/main/java/icesi/edu/co/fitscan/features/home/data/dataSources/IDashboardDataSource.kt
+++ app/src/main/java/icesi/edu/co/fitscan/features/home/data/dataSources/IDashboardDataSource.kt
@@ -1,6 +1,6 @@
 package icesi.edu.co.fitscan.features.home.data.dataSources
 
-import icesi.edu.co.fitscan.domain.model.DashboardResponse
+import icesi.edu.co.fitscan.features.workout.data.dto.WorkoutsResponse
 import retrofit2.Response
 import retrofit2.http.*
 
@@ -14,5 +14,5 @@ interface IDashboardDataSource {
     @GET("$BASE_PATH/workout")
     suspend fun getDashboardData(
         @Query("filter[customer_id][_eq]") userId: String
-    ): Response<DashboardResponse>
+    ): Response<WorkoutsResponse>
 }
--- app/src/main/java/icesi/edu/co/fitscan/features/home/data/repositories/DashboardRepositoryImpl.kt
+++ app/src/main/java/icesi/edu/co/fitscan/features/home/data/repositories/DashboardRepositoryImpl.kt
@@ -3,16 +3,23 @@ package icesi.edu.co.fitscan.features.home.data.repositories
 import icesi.edu.co.fitscan.domain.model.DashboardResponse
 import icesi.edu.co.fitscan.domain.repositories.IDashboardRepository
 import icesi.edu.co.fitscan.features.home.data.dataSources.IDashboardDataSource
+import icesi.edu.co.fitscan.features.workout.data.mapper.WorkoutMapper
 
 class DashboardRepositoryImpl(
     private val datasource: IDashboardDataSource
 ) : IDashboardRepository {
 
+    private val workoutMapper = WorkoutMapper()
+
     override suspend fun getDashboardData(userId: String): Result<DashboardResponse> {
         return try {
             val response = datasource.getDashboardData(userId = userId)
             if (response.isSuccessful) {
-                Result.success(response.body()!!)
+                val workoutsResponse = response.body()!!
+                // Mapear de WorkoutsResponse a DashboardResponse
+                val workouts = workoutsResponse.data.map { dto -> workoutMapper.toDomain(dto) }
+                val dashboardResponse = DashboardResponse(data = workouts)
+                Result.success(dashboardResponse)
             } else {
                 Result.failure(Exception("Error: ${response.code()} - ${response.errorBody()?.string()}"))
             }
